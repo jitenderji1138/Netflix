e77059682a68f2f95589f753f24414d9af0c2d9b
jitenderji1138 <131745277+jitenderji1138@users.noreply.github.com>
2023-04-25 16:51:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // server.js
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("https://jitenderji1137.github.io/Netflix/db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 server.use(middlewares);
